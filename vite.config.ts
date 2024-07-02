1d636c214855c56ac205374a32e8b701f3a4d264
Samirgog <44999589+Samirgog@users.noreply.github.com>
2024-07-02 16:08:04 +0700

    Git hub pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "/",
+  base: "/button/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }]
   }
