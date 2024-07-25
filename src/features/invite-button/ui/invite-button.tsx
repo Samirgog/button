d843c168bee0a1c1f8e845e0e49f7624c4df8a0d
ivan_maslov <maslov@echo-company.ru>
2024-07-25 17:07:39 +0500

    fix: referral link

--- src/features/invite-button/ui/invite-button.tsx
+++ src/features/invite-button/ui/invite-button.tsx
@@ -1,53 +1,72 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { userModel } from "@/entities/user";
 import { Button, Emodji, emodjiTypes, Stack, Typography } from "@/shared/ui";
 
+const TEXT = "\nHi, friend! Join and earn buttons! Nothing but ton... Do you get it? 🤪🥖";
+
 export const InviteButton: React.FC = () => {
-  const [isSharing, setIsSharing] = useState(false);
   const user = userModel.useUser();
 
-  const handleClick = () => {
-    if (user?.telegramId) {
-      const referralLink = `https://t.me/nothing_but_ton_bot?start=referralId=${user?.telegramId}`;
+  const URl = `https://t.me/nothing_but_ton_bot?start=referralId${user?.telegramId}`;
+  const referralLink = `https://t.me/share/url?url=${encodeURI(URl)}&text=${encodeURI(TEXT)}`;
 
-      setIsSharing(true);
+  console.log("referralLink", referralLink);
+  // const handleClick = () => {
+  //   if (user?.telegramId) {
+  //     // const referralLink = `https://t.me/nothing_but_ton_bot?start=referralId=${user?.telegramId}`;
+  //     const url = 'https://t.me/nothing_but_ton_bot';
+  //     const text = 'Hi, friend! Join and earn buttons! Nothing but ton... Do you get it? 🤪🥖 from text';
+  //     const referralLink = `https://t.me/share/url?url=${encodeURI(url)}&text=${encodeURI(text)}`;
+  //
+  //       setIsSharing(true);
+  //
+  //     if (navigator.share) {
+  //       navigator
+  //         .share({
+  //           title: "Пригласить друга",
+  //           text: "Присоединяйся ко мне выполняй задания, кроши батон и богатей!",
+  //           url: referralLink
+  //         })
+  //         .catch((error) => console.error("Error sharing", error))
+  //         .finally(() => setIsSharing(false));
+  //     } else {
+  //       navigator.clipboard
+  //         .writeText(referralLink)
+  //         .then(() => {
+  //           alert("Ссылка скопирована в буфер обмена");
+  //         })
+  //         .catch((error) => console.error("Error copying link", error))
+  //         .finally(() => setIsSharing(false));
+  //     }
+  //   }
+  // };
 
-      if (navigator.share) {
-        navigator
-          .share({
-            title: "Пригласить друга",
-            text: "Присоединяйся ко мне выполняй задания, кроши батон и богатей!",
-            url: referralLink
-          })
-          .catch((error) => console.error("Error sharing", error))
-          .finally(() => setIsSharing(false));
-      } else {
-        navigator.clipboard
-          .writeText(referralLink)
-          .then(() => {
-            alert("Ссылка скопирована в буфер обмена");
-          })
-          .catch((error) => console.error("Error copying link", error))
-          .finally(() => setIsSharing(false));
-      }
-    }
-  };
+  // return (
+  //   <Button
+  //     type="gradient"
+  //     size="md"
+  //     onClick={handleClick}
+  //     disabled={isSharing}
+  //     style={{ width: "100%" }}
+  //   >
+  //     <Stack gap={8} justify="center" align="center">
+  //       <Typography type="title" weight="bold">
+  //         Invite a friend
+  //       </Typography>
+  //       <Emodji emodjiName={emodjiTypes.EmodjiName.FRIEND} size={24} />
+  //     </Stack>
+  //   </Button>
+  // );
 
   return (
-    <Button
-      type="gradient"
-      size="md"
-      onClick={handleClick}
-      disabled={isSharing}
-      style={{ width: "100%" }}
-    >
+    <a href={referralLink}>
       <Stack gap={8} justify="center" align="center">
         <Typography type="title" weight="bold">
           Invite a friend
         </Typography>
         <Emodji emodjiName={emodjiTypes.EmodjiName.FRIEND} size={24} />
       </Stack>
-    </Button>
+    </a>
   );
 };
